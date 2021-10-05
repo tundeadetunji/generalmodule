af8073aad4768d3e99f3dc900964b8452223631a
tundeadetunji <tundeadetunji2017@gmail.com>
2021-10-05 18:22:13 +0100

    w/ Clipboard

---
+++ GeneralModule/src/main/java/com/inovationware/generalmodule/Machine.java
@@ -0,0 +1,39 @@
+package com.inovationware.generalmodule;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+public class Machine {
+
+    Context context;
+
+    public Machine(Context context) {
+        this.context = context;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    void ClipboardSetText(String text_to_copy) {
+        ClipboardManager clipboard = (ClipboardManager) context.getSystemService(context.CLIPBOARD_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            clipboard.clearPrimaryClip();
+        }
+        ClipData clip = ClipData.newPlainText("", text_to_copy);
+        clipboard.setPrimaryClip(clip);
+    }
+
+    public String ClipboardGetText() {
+        String clipboardText;
+        ClipboardManager clipboard = (ClipboardManager) context.getSystemService(context.CLIPBOARD_SERVICE);
+
+        clipboardText = clipboard.getPrimaryClip().getItemAt(0)
+                .coerceToText(context).toString();
+
+        return clipboardText;
+    }
+
+
+}
