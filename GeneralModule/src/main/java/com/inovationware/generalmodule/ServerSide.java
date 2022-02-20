848694ffc6744f8f390efd9263f65f8be959f743
Tunde Adetunji <tundeadetunji2017@gmail.com>
2022-02-21 00:00:38 +0100

    ServerSide methods

--- GeneralModule/src/main/java/com/inovationware/generalmodule/ServerSide.java
+++ GeneralModule/src/main/java/com/inovationware/generalmodule/ServerSide.java
@@ -6,19 +6,22 @@ import android.util.Log;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ServerSide {
-    private static String _user;
-    private static String _pass;
-    private static String _DB;
-    private static String _server;
+    private static String username;
+    private static String password;
+    private static String database;
+    private static String server;
 
-    public ServerSide(String _user, String _pass, String _DB, String _server) {
-        this._user = _user;
-        this._pass = _pass;
-        this._DB = _DB;
-        this._server = _server;
+    public ServerSide(String server, String database, String username, String password) {
+        this.server = server;
+        this.database = database;
+        this.username = username;
+        this.password=password;
     }
 
     @SuppressLint("NewApi")
@@ -31,19 +34,65 @@ public class ServerSide {
         String ConnURL = null;
         try {
             Class.forName("net.sourceforge.jtds.jdbc.Driver");
-            ConnURL = "jdbc:jtds:sqlserver://" + _server + ";"
-                    + "databaseName=" + _DB + ";user=" + _user + ";password="
-                    + _pass + ";";
+            ConnURL = "jdbc:jtds:sqlserver://" + server + ";"
+                    + "databaseName=" + database + ";user=" + username + ";password="
+                    + password + ";";
             conn = DriverManager.getConnection(ConnURL);
         } catch (SQLException se) {
-            Log.e("ERRO", se.getMessage());
+            //Log.e("ERRO", se.getMessage());
         } catch (ClassNotFoundException e) {
-            Log.e("ERRO", e.getMessage());
+            //Log.e("ERRO", e.getMessage());
         } catch (Exception e) {
-            Log.e("ERRO", e.getMessage());
+            //Log.e("ERRO", e.getMessage());
         }
         return conn;
     }
 
+    private void writeValue(String value){
+        try {
+//            ServerSide serverSideConnect = new ServerSide(getString(R.string._user), getString(R.string._pass), getString(R.string._DB), getString(R.string._server));
+            Connection connect = ServerSide.con();
+
+            String queryStmt = "UPDATE Windowsapplication999_Settings SET Settings='" + value + "'";
+
+            PreparedStatement preparedStatement = connect
+                    .prepareStatement(queryStmt);
+
+            preparedStatement.executeUpdate();
+
+            preparedStatement.close();
+
+        } catch (SQLException e) {
+            //textDetails.setText("SQLException\n\n" + e.getMessage().toString());
+        } catch (Exception e) {
+            //textDetails.setText("Exception\n\n" + e.getMessage().toString());
+        }
+
+    }
+
+    private Object readValue(){
+        Object settings = null;
+        try {
+            Connection connect = ServerSide.con();
+
+            String queryStmt = "SELECT Settings FROM Windowsapplication999_Settings ORDER BY RecordSerial ASC";
+
+            Statement statement = connect.createStatement();
+            ResultSet rows = statement.executeQuery(queryStmt);
+
+            while(rows.next()){
+                settings = rows.getString("Settings");
+            }
+            statement.close();
+        }
+        catch (SQLException e) {
+            //textDetails.setText("SQLException\n\n" + e.getMessage().toString());
+        } catch (Exception e) {
+            //textDetails.setText("Exception\n\n" + e.getMessage().toString());
+        }
+
+        return settings;
+
+    }
 
 }
