113a64d41e17a4be863ab6b1a332410418940d06
Tunde Adetunji <tundeadetunji2017@gmail.com>
2022-02-20 23:17:06 +0100

    new ServerSide

--- GeneralModule/src/main/java/com/inovationware/generalmodule/ServerSide.java
+++ GeneralModule/src/main/java/com/inovationware/generalmodule/ServerSide.java
@@ -1,102 +1,42 @@
 package com.inovationware.generalmodule;
 
-import android.widget.TextView;
+import android.annotation.SuppressLint;
+import android.os.StrictMode;
+import android.util.Log;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ServerSide {
+    @SuppressLint("NewApi")
+    public static Connection con() {
 
-    private String username;
-    private String password;
-    private String app_name;
-
-    private String baseUrl;
-    private String directory;
-    private String code;
-
-    private String reply;
-
-    public ServerSide(String baseUrl, String directory, String code, String username, String password, String app_name) {
-        this.username = username;
-        this.password = password;
-        this.app_name = app_name;
-        this.baseUrl = baseUrl;
-        this.directory = directory;
-        this.code = code;
-
-
-    }
-
-    public void readValue(String file, TextView textView) {
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(baseUrl)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-
-        IReadModelAPI readmodelAPI = retrofit.create(IReadModelAPI.class);
-        Call<ReadModel> call = readmodelAPI.getContentFromServer(toPath(baseUrl), toPath(directory), code, username, password, file, app_name);
-        call.enqueue(new Callback<ReadModel>() {
-            @Override
-            public void onResponse(Call<ReadModel> call, Response<ReadModel> response) {
-                if (response.isSuccessful()) {
-                    ReadModel dataFromServer = response.body();
-                    String status = dataFromServer.getStatus();
-                    String data = dataFromServer.getData();
-                    //reply = data;
-                    textView.setText(data);
-                } else {
-                    //textDetails.setText("Failed with code " + response.code());
-                }
-            }
-
-            @Override
-            public void onFailure(Call<ReadModel> call, Throwable t) {
-                //textDetails.setText(t.getMessage());
-            }
-        });
-    }
-
-    public void writeValue(String file, Boolean append, String store, String text) {
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(baseUrl)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-
-        IWriteModelAPI writemodelAPI = retrofit.create(IWriteModelAPI.class);
-        Call<WriteModel> call = writemodelAPI.getContentFromServer(toPath(baseUrl), toPath(directory), code, username, password, file, app_name, append, store, text);
-        call.enqueue(new Callback<WriteModel>() {
-            @Override
-            public void onResponse(Call<WriteModel> call, Response<WriteModel> response) {
-                if (response.isSuccessful()) {
-                    WriteModel dataFromServer = response.body();
-                    String status = dataFromServer.getStatus();
-                    String data = dataFromServer.getData();
-
-                } else {
-                    //textDetails.setText("Failed with code " + response.code());
-                }
-            }
-
-            @Override
-            public void onFailure(Call<WriteModel> call, Throwable t) {
-                //textDetails.setText(t.getMessage());
-            }
-        });
-    }
-
-    private String toPath(String string) {
-        String result = "";
-        if (string.endsWith("/")) {
-            result = string;
-        } else {
-            result = string + "/";
+        String _user = "User2008";
+        String _pass = "cvwdxnhfAv754ilwE_6";
+        String _DB = "Stock2008";
+        //String _server = "41.190.2.102:1500";
+        String _server = "mssql.inovationware.com:1500";
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection conn = null;
+        String ConnURL = null;
+        try {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            ConnURL = "jdbc:jtds:sqlserver://" + _server + ";"
+                    + "databaseName=" + _DB + ";user=" + _user + ";password="
+                    + _pass + ";";
+            conn = DriverManager.getConnection(ConnURL);
+        } catch (SQLException se) {
+            Log.e("ERRO", se.getMessage());
+        } catch (ClassNotFoundException e) {
+            Log.e("ERRO", e.getMessage());
+        } catch (Exception e) {
+            Log.e("ERRO", e.getMessage());
         }
-        return result;
+        return conn;
     }
+
+
 }
